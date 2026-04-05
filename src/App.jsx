0be2e8b13be3722ea3fe4c4b49e90cb48cea2c8e
Sid Ganti <gantisiddhartha444@gmail.com>
2026-04-05 09:24:50 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/navbar.jsx";
+import {navLinks} from "./constants/index.js";
+
+const App = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="logo"/>
+                <ul>
+                    {navLinks.map(({ label})=>(
+                        <li key={label}>
+                        <a href={label}>{label}</a>
+                        </li>
+                        )
+                    )}
+                </ul>
+                <div className="flex-center gap-3" >
+                    <button>
+                        <img src="/search.svg" alt="search"/>
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="cart"/>
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>NavBar</div>
+    )
+}
+export default Navbar
